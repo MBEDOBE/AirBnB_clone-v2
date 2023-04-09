391146841ccb34126bbe192895d2b264aebeda41
habiibullah <ajiboyhabiib@gmail.com>
2023-04-09 20:45:45 +0000

    Write a Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean:

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,22 +1,60 @@
 #!/usr/bin/python3
-""" Function that deploys """
-from fabric.api import *
-
-
-env.hosts = ['35.168.2.117', '54.146.9.154']
-env.user = "ubuntu"
-
-
+"""
+Fabric script methods:
+    do_pack: packs web_static/ files into .tgz archive
+    do_deploy: deploys archive to webservers
+    deploy: do_packs && do_deploys
+Usage:
+    fab -f 3-deploy_web_static.py deploy -i my_ssh_private_key -u ubuntu
+"""
+from fabric.api import local, env, put, run
+from time import strftime
+import os.path
+env.hosts = ['18.209.178.197', '18.204.20.146']
+def do_pack():
+    """generate .tgz archive of web_static/ folder"""
+    datetime = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        file_name = "versions/web_static_{}.tgz".format(datetime)
+        local("tar -cvzf {} web_static/".format(file_name))
+        return file_name
+    except:
+        return None
+def do_deploy(archive_path):
+    """
+    Deploy archive to web server
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, path_no_ext))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
+        return True
+    except:
+        return False
+def deploy():
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    success = do_deploy(archive_path)
+    return success
 def do_clean(number=0):
-    """ CLEANS """
-
-    number = int(number)
-
     if number == 0:
-        number = 2
-    else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+        number = 1
+    with cd.local('./versions'):
+            local("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
+            xargs -d '\n' rm".format(1 + number))
+    with cd('/data/web_static/releases/'):
+            run("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
+            xargs -d '\n' rm".format(1 + number))
