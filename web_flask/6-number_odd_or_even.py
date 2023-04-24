1fb31d71cbaa5ee19cff5c0bdc355b44f48152ef
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-24 08:54:16 +0000

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,43 +1,56 @@
+#!/usr/bin/python3
+"""Flask module"""
+
 from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
+    """Greetings, hbnb project"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return 'C {}'.format(text.replace('_', ' '))
+def text_c(text):
+    """Displays 'C' followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return "C {}".format(escape(text))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    return 'Python {}'.format(text.replace('_', ' '))
+def textpython(text='is cool'):
+    """Displays 'Python' followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(escape(text))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return '{} is a number'.format(n)
+    """displays “<n> is a number” only if n is an integer"""
+    return "{} is a number".format(escape(n))
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """displays “<n> is a number” only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
+    """displays “<n> is a number” only if n is an integer"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
