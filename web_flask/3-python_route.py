62ae5318a853f728f558c21c2d4e78d054115568
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-24 08:31:19 +0000

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,6 +11,7 @@ Routes:
 """
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -18,13 +19,13 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
-    return "HBNB"
+    return 'HBNB'
 
 
 @app.route("/c/<text>", strict_slashes=False)
@@ -33,12 +34,12 @@ def c_text(text):
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text):
+@app.route('/python/', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
     """Displays 'Python ' followed by the value of the text variable."""
     return "Python {}".format(text.replace('_', ' '))
 
 
-if __name__ == "__main__":
+if __name__ == "__main__':
     app.run(host="0.0.0.0", port=5000)
