a6779f1f0fd197eab7dffb549de713ecfae92b82
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-24 08:39:53 +0000

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,44 @@
 #!/usr/bin/python3
-"""Flask module"""
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: Displays 'HBNB'
+    /c/<text>: Displays 'C ' followed by the value of the text variable.
+    /python/(<text>): Displays 'Python ' followed by the value of the text variable.
+"""
 
 from flask import Flask
-from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """Greetings, hbnb project"""
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays HBNB"""
-    return 'HBNB'
+    """Displays 'HBNB'"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def text_c(text):
-    """Displays 'C' followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return "C {}".format(escape(text))
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Displays 'C ' followed by the value of the text variable."""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def textpython(text='is cool'):
-    """Displays 'Python' followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return "Python {}".format(escape(text))
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    """Displays 'Python ' followed by the value of the text variable."""
+    return "Python {}".format(text.replace('_', ' '))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
