114a523d8ad27d9103451a83b270df72cc4c2a37
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-24 08:49:48 +0000

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
 Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
 
-Routes:
-    /: Displays 'Hello HBNB!'
-    /hbnb: Displays 'HBNB'
-    /c/<text>: Displays 'C ' followed by the value of the text variable.
-    /python/(<text>): Displays 'Python ' followed by the value of the text variable.
 """
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -30,14 +25,15 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """Displays 'C ' followed by the value of the text variable."""
-    return "C {}".format(text.replace('_', ' '))
+    return "C {}".format(escape(text).replace('_', ' '))
 
 
-@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+
+@app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text='is cool'):
     """Displays 'Python ' followed by the value of the text variable."""
-    return "Python {}".format(text.replace('_', ' '))
+    return "Python {}".format((text).replace('_', ' '))
 
 
 if __name__ == "__main__":
